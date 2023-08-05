ebac4e60e04b43fd799fae8aa69d371124802790
lee-zq <zq_lee@163.com>
2023-08-05 17:16:06 +0800

    [create mydev branch]

--- mmdet/utils/__init__.py
+++ mmdet/utils/__init__.py
@@ -10,11 +10,13 @@ from .replace_cfg_vals import replace_cfg_vals
 from .setup_env import setup_multi_processes
 from .split_batch import split_batch
 from .util_distribution import build_ddp, build_dp, get_device
+from .rfnext import rfnext_init_model
 
 __all__ = [
     'get_root_logger', 'collect_env', 'find_latest_checkpoint',
     'update_data_root', 'setup_multi_processes', 'get_caller_name',
     'log_img_scale', 'compat_cfg', 'split_batch', 'build_ddp', 'build_dp',
     'get_device', 'replace_cfg_vals', 'AvoidOOM', 'AvoidCUDAOOM',
-    'get_max_num_gt_division_factor', 'masked_fill', 'batch_images_to_levels'
+    'get_max_num_gt_division_factor', 'masked_fill', 'batch_images_to_levels',
+    "rfnext_init_model"
 ]
